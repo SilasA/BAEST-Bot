2274e7c2e3792163194f5bbc29a6876014f44425
SilasA <agnewsilas@gmail.com>
2016-11-10 17:32:20 -0500

    Added Connecting functionality

--- BAEST Bot/Bot.cs
+++ BAEST Bot/Bot.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Net;
+using Discord;
 
 namespace BAEST_Bot
 {
@@ -12,12 +13,19 @@ namespace BAEST_Bot
     /// </summary>
     class Bot
     {
+        private DiscordClient client;
+
         /// <summary>
         /// 
         /// </summary>
         public Bot()
         {
-
+            client = new DiscordClient();
+            client.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
+                    await e.Channel.SendMessage(e.Message.Text);
+            };
         }
 
         /// <summary>
@@ -25,7 +33,12 @@ namespace BAEST_Bot
         /// </summary>
         public void Run()
         {
-
+            //Convert our sync method to an async one and block the Main function until the bot disconnects
+            client.ExecuteAndWait(async () =>
+            {
+                //Connect to the Discord server using our email and password
+                await client.Connect("discordtest@email.com", "Password123");
+            });
         }
     }
 }
